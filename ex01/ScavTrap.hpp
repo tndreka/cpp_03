71ac71027f612f821ad52f88c380b67e8a3cd0b8
tndreka <tndreka@student.42heilbronn.de>
2025-07-11 18:01:18 +0200

    ADD: to access the clapttrap i inherit to this clas and initialize in canonical form

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,24 +6,25 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 18:10:55 by tndreka           #+#    #+#             */
-/*   Updated: 2025/07/10 18:13:20 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/11 17:58:35 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP 
 
-#include <iostream>
+#include "ClapTrap.hpp"
 
-class ScavTrap
+class ScavTrap : public ClapTrap
 {
-private:
-	
-public:
-	ScavTrap();
-	ScavTrap(const ScavTrap& other);
-	ScavTrap& operator=(const ScavTrap& other);
-	~ScavTrap();
+	public:
+		ScavTrap();
+		ScavTrap(const ScavTrap& other);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& other);
+		~ScavTrap();
+
+		void guardGate();
 };
 
 #endif
